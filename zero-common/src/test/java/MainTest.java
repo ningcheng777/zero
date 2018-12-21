ef30bfd4dcbedb811be56af361cdfd52290fdf6a
ningcheng <1520274124@qq.com>
2018-12-21 14:35:41 +0800

    update

--- zero-common/src/test/java/MainTest.java
+++ zero-common/src/test/java/MainTest.java
@@ -5,23 +5,8 @@
 public class MainTest {
 
     public static void main(String[] args) {
-        A a = new A();
-    }
-
-    public static class A {
-
-        {
-            System.out.println(2);
-        }
-
-        {
-            System.out.println(3);
-        }
-
-        public A() {
-            System.out.println(1);
-        }
 
     }
+
 
 }
