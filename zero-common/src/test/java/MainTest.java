eb9d7fb62b93965a97284d3b5783c3d6ce08885e
ningcheng <1520274124@qq.com>
2019-01-02 09:57:39 +0800

    update

--- zero-common/src/test/java/MainTest.java
+++ zero-common/src/test/java/MainTest.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author ningcheng
  * @date 2018/1/25
@@ -5,7 +8,18 @@
 public class MainTest {
 
     public static void main(String[] args) {
-
+//        ExecutorService service = Executors.newFixedThreadPool(1);
+        Runnable r = () -> {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(1);
+        };
+//        service.execute(r);
+        new Thread(r).start();
+        System.out.println(2);
     }
 
 }
